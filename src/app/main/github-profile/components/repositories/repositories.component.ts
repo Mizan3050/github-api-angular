f53487dd5c35b1b88b3951c7ff2e8961aed5833b
Meezan Shaikh <meezan@tradexa.co.in>
2024-02-16 23:06:29 +0530

    repository list updated

--- src/app/main/github-profile/components/repositories/repositories.component.ts
+++ src/app/main/github-profile/components/repositories/repositories.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { GithubRepositoryService } from 'src/app/main/github-profile/services/github-repository.service';
 import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-repositories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './repositories.component.html',
   styleUrls: ['./repositories.component.scss']
 })
 export class RepositoriesComponent {
 
   githubProfile$ = this.githubRepositoryService.githubProfile$;
+  startIcon = '/assets/images/icons/icons8-star-48.png'
 
   constructor(
     private githubRepositoryService: GithubRepositoryService
