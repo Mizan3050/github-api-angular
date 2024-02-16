279af3ef2382e362c529bc0f8813b072ef90a82a
Meezan Shaikh <meezan@tradexa.co.in>
2024-02-16 17:57:31 +0530

    github profile detail binding implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: ':username',
-    loadComponent: () => import('./main/github-profile/components/github-profile/github-profile.component').then(comp => comp.GithubProfileComponent)
+    loadComponent: () => import('./main/github-profile/components/github-profile/github-profile.component').then(comp => comp.GithubProfileComponent),
+  },
+  {
+    path: ':username/repositories',
+    loadComponent: () => import('./main/github-profile/components/repositories/repositories.component').then(comp => comp.RepositoriesComponent)
   }
 ];
 
--- src/app/main/github-profile/components/github-profile/github-profile.component.ts
+++ src/app/main/github-profile/components/github-profile/github-profile.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { GithubProfile } from 'src/app/main/github-profile/interface/github-profile';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-github-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatProgressSpinnerModule, RouterModule],
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.scss']
 })
 export class GithubProfileComponent implements OnInit {
 
   username: string = '';
+  profileLoading = true;
+  profileIcon='/assets/images/icons/icons8-test-account-30.png'
+  repoIcon='/assets/images/icons/icons8-bookmark-48.png'
+
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute
@@ -26,7 +31,11 @@ export class GithubProfileComponent implements OnInit {
 
   ngOnInit() {
     this.github$ = this.http.get<GithubProfile>(`https://api.github.com/users/${this.username}`).pipe(
+      tap(() => {
+        this.profileLoading = false;
+      }),
       catchError(() => {
+        this.profileLoading = false;
         return of(null)
       })
     );
