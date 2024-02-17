dc273a426d9aa51ee18c9481591529cdd82ec853
Meezan Shaikh <meezan@tradexa.co.in>
2024-02-17 17:17:12 +0530

    search profile feature added on home page

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  searchControl = new FormControl('');
 
+  private router = inject(Router);
+
+  searchProfile() {
+    if (this.searchControl.value) {
+      this.router.navigate([this.searchControl.getRawValue()])
+    }
+  }
 }
