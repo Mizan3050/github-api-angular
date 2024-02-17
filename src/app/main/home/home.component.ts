fa7be0cc96780f743fa66160b1c54dd2662b3c75
Meezan Shaikh <meezan@tradexa.co.in>
2024-02-17 20:11:23 +0530

    description updated

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   searchControl = new FormControl('');
   private router = inject(Router);
-
+  projectDescription = `With this project, users can effortlessly search for a GitHub profile and retrieve a detailed list of its repositories. The application leverages the GitHub API to provide real-time information on repositories, including their names, descriptions and stars.`;
   searchProfile() {
     if (this.searchControl.value) {
       this.router.navigate([this.searchControl.getRawValue()])
